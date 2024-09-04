2365c5e4b681c4d471f57e614ecaea5fdd6a7fa8
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-09-04 23:59:32 +0000

    3 projects completed

---
+++ 02.js
@@ -0,0 +1,5 @@
+function  sum(num1,num2){
+    add = num1+num2;
+    return add;
+}
+console.log(sum(122,13));
---
+++ 03.js
@@ -0,0 +1,8 @@
+const printNumber = (n)=> {
+   for (let index = 0; index < n; index++) {
+    console.log( index);
+    
+
+   }
+}
+printNumber(6);
